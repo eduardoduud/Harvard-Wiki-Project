53c4d45f65a79d1ccbaa816f35808816b2cf019b
Eduardo <eeadomingues@gmail.com>
2024-03-13 22:59:57 -0300

    add search feature

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from . import util
 
@@ -14,3 +14,19 @@ def entries(request, title):
         "title": title
     })
 
+def search(request):
+    if request.method == "GET":
+        title = request.GET['q']
+        if title:
+            entries = util.list_entries()
+            for entry in entries:
+                if title == entry:
+                    return redirect('entries', title=title)
+            substring_list = []
+            for entry in entries:
+                if title.lower() in entry.lower():
+                    substring_list.append(entry)
+            return render(request, "encyclopedia/search.html", {
+                "entries": substring_list,
+                "title": title
+            })
